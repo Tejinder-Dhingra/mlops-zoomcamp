3f53851f2129706ba329ae78f8a1a587becfca5c
Tejinder Pal Singh Dhingra <tejinder_pal_singh_dhingra@epam.com>
2025-04-01 23:11:09 +0530

    Parametrized homework.py

--- 04-deployment/homework/homework.py
+++ 04-deployment/homework/homework.py
@@ -12,19 +12,6 @@ import json
 from glob import glob
 from argparse import ArgumentParser
 
-month = 3
-year = 2023
-input_dir = 'input'
-output_dir = 'output'
-trip_category = 'yellow'
-
-python_version = platform.python_version()
-filename = f'{trip_category}_tripdata_{year}-{month:02d}.parquet'
-input_file_url = f'https://d37ci6vzurychx.cloudfront.net/trip-data/{filename}'
-output_file = f'{output_dir}/{filename}'
-
-categorical = ['PULocationID', 'DOLocationID']
-
 # ## Q5. Parametrizing the script
 def parse_args():
     parser = ArgumentParser(prog='deployment', usage='%(prog)s [options]')
@@ -40,52 +27,48 @@ def read_data(filename):
 
     df = df[(df.duration >= 1) & (df.duration <= 60)].copy()
 
+    categorical = ['PULocationID', 'DOLocationID']
     df[categorical] = df[categorical].fillna(-1).astype('int').astype('str')
 
     return df
 
-# ## Training model
+if __name__ == '__main__':
+    args = parse_args()
+    if args.year: year = int(args.year)
+    if args.month: month = int(args.month)
 
-if not os.path.exists(input_dir): 
-    os.makedirs(input_dir)
-    input_file = wget.download(input_file_url, out=input_dir)
-else:
-    glob_str = f'{input_dir}/*.parquet'
-    input_file = glob(glob_str)
+    input_dir = 'input'
+    trip_category = 'yellow'
 
-df = read_data(input_file)
-with open('model.bin', 'rb') as f_in:
-    dv, model = pickle.load(f_in)
+    python_version = platform.python_version()
+    filename = f'{trip_category}_tripdata_{year}-{month:02d}.parquet'
+    input_file_url = f'https://d37ci6vzurychx.cloudfront.net/trip-data/{filename}'
 
-dicts = df[categorical].to_dict(orient='records')
-X_val = dv.transform(dicts)
-y_pred = model.predict(X_val)
+    # ## Training model
 
-# ## Q1. Standard deviation
+    if not os.path.exists(input_dir): 
+        os.makedirs(input_dir)
+        print(f'Downloading {filename}...')
+        input_file = wget.download(input_file_url, out=input_dir)
+    else:
+        glob_str = f'{input_dir}/*.parquet'
+        input_file = glob(glob_str)
 
-print((f'Standard Deviation = {y_pred.std():.3f}'))
+    df = read_data(input_file)
+    with open('model.bin', 'rb') as f_in:
+        dv, model = pickle.load(f_in)
 
-# ## Q2. Preparing the output
+    categorical = ['PULocationID', 'DOLocationID']
+    dicts = df[categorical].to_dict(orient='records')
+    X_val = dv.transform(dicts)
+    y_pred = model.predict(X_val)
 
-df_result = pd.DataFrame()
-df_result['ride_id'] = f'{year:04d}/{month:02d}_' + df.index.astype('str')
-df_result['predicted_duration'] = y_pred
+    df_result = pd.DataFrame()
+    df_result['ride_id'] = f'{year:04d}/{month:02d}_' + df.index.astype('str')
+    df_result['predicted_duration'] = y_pred
 
-if os.path.exists(output_file):
-    os.remove(output_file)
-else:
-    if not os.path.exists(output_dir): os.makedirs(output_dir)
-df_result.to_parquet(
-    output_file,
-    engine = 'pyarrow',
-    compression = None,
-    index = False,
-)
-print(f'{filename} - {(os.path.getsize(output_file)/1024**2):.2f}M')
+    print('Q5. Parametrizing script and Mean predicted duration')
+    print(f'Parameters: year={year}, month={month}')
+    print(f'Input file: {input_file}')
+    print(f'Mean predicted duration = {y_pred.mean():.3f}')
 
-with open('Pipfile.lock', 'r') as f:
-    lock_data = json.load(f)
-print(f'First hash of Scikit-learn dependency = "{lock_data['default']['scikit-learn']['hashes'][0]}"')
-
-args = parse_args()
-print(args)
